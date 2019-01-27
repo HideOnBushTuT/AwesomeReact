458b4977514bf885598de1605055d7f52917ef14
shanbin cai <shanbin.cai@gmail.com>
2019-01-27 23:06:41 +0800

    Add useEffect to fetch person info

--- src/App/Hooks/InputWithHooks.js
+++ src/App/Hooks/InputWithHooks.js
@@ -18,13 +18,30 @@ import React, { useState, useEffect } from 'react';
 //     );
 // }
 
+const useFetch = (url) => {
+    const [person, setPerson] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     document.title = `You Click ${count} times`;
+    // }, [count]);
+
+    useEffect(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        const [item] = data.results;
+        setPerson(item);
+        setLoading(false);
+    }, []);
+
+    return {person, loading};
+}
+
 export default () => {
     const [name, setName] = useState('Google');
     const [count, setCount] = useState(0);
+    const {person, loading} = useFetch('https://api.randomuser.me/');
 
-    useEffect(() => {
-        document.title = `You Click ${count} times`;
-    }, []);
 
     return (
         <div className="App" style={{justifyContent: 'center', alignItems: 'center'}}>
@@ -37,6 +54,7 @@ export default () => {
                 onChange={(e) => setName(e.target.value)}
             />
             <button onClick={() => setCount(count + 1)}>Ckick Me To Plus One</button>
+            {loading ? <div>loading...</div> : <div>{person.name.first}</div>}
         </div>
     );
 }
