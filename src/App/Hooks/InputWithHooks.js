16003916e78bdfc2eec92619d6631d590f4c6649
shanbin cai <shanbin.cai@gmail.com>
2019-01-31 17:26:23 +0800

    Change Style

--- src/App/Hooks/InputWithHooks.js
+++ src/App/Hooks/InputWithHooks.js
@@ -22,14 +22,14 @@ import { AsyncSeriesWaterfallHook } from 'tapable';
 const initialState = { count: 0 };
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'reset': 
+    switch (action.type) {
+        case 'reset':
             return { count: action.payload };
         case 'increment':
-            return { count: state.count + 1}
+            return { count: state.count + 1 }
         case 'decrement':
             return { count: state.count - 1 }
-        default: 
+        default:
             return state;
     }
 }
@@ -53,14 +53,14 @@ const useFetch = (url) => {
     return { person, loading };
 }
 
-const names = ({name}) => {
+const names = ({ name }) => {
     const nameView = useMemo(() => <div>{name}</div>, [name]);
     return <div>
         {nameView}
     </div>
 }
 
-export default ({initialcount}) => {
+export default ({ initialcount }) => {
     const [name, setName] = useState('Google');
     // const [count, setCount] = useState(0);
     const [state, dispatch] = useReducer(reducer, initialState, { type: 'reset', payload: initialcount });
@@ -84,11 +84,11 @@ export default ({initialcount}) => {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            <button onClick={() => dispatch({type: 'increment'})}>+</button>
-            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-            <button onClick={() => dispatch({type: 'reset', payload: 100})}>reset</button>
+            <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+            <button onClick={() => dispatch({ type: 'reset', payload: 100 })}>reset</button>
             {/* {loading ? <div>loading...</div> : <div>{person.name.first}</div>} */}
-            { names({name: loading ? 'loading...' : person.name.first})}
+            {names({ name: loading ? 'loading...' : person.name.first })}
         </div>
     );
 }
