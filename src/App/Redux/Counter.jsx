78a772fc11a469d6bd48933f871dcdd574b40e1d
shanbin cai <shanbin.cai@gmail.com>
2019-02-20 16:55:15 +0800

    Add redux count

---
+++ src/App/Redux/Counter.jsx
@@ -0,0 +1,68 @@
+import  React from "react";
+import {  } from "react-dom";
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
+import { dispatch } from "rxjs/internal/observable/pairs";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const increment = () => {
+    return {
+        type: INCREMENT,
+    };
+};
+
+const decrement = () => {
+    return {
+        type: DECREMENT,
+    };
+};
+
+const CounterReducer = (state, action) =>{
+
+    switch(action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT: 
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+
+const initValue = 0;
+const store = createStore(CounterReducer, initValue, applyMiddleware(logger));
+
+const Counter = ({count, onIncrement, onDecrement}) => {
+    return (
+        <div>
+        <div>{count}</div>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        count: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIncrement: () => dispatch(increment()),
+        onDecrement: () => dispatch(decrement()),
+    };
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+export default () => {
+    return <Provider store={store}>
+        <CounterContainer/>
+    </Provider>
+}
---
+++ src/App/Todos/Todo.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import { observer } from "mobx-react";
+
+// @observer
+// class TimerView extends React.Component {
+//     render() {
+//         return (
+//             <button>
+//                 Seconds passed: { this.props.appState.timer }
+//             </button>
+//         );
+//     }
+
+//     onReset() {
+//         this.props.appState.resetTimer();
+//     }
+// }
+
+// export default TimerView;
+
