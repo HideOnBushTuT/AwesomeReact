79053e09719bb81c8a4e3e4a8816b1cc5bf20a84
shanbin cai <shanbin.cai@gmail.com>
2019-02-26 10:28:46 +0800

    Add Immutable basic

--- src/App/Immutable/Immutable.jsx
+++ src/App/Immutable/Immutable.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+
+import { Map } from 'immutable';
+
+const map1 = Map({ a: 1, b: 2, c:3 });
+const map2 = map1.set('b', 50);
+
+const immutableTEst = () => {
+    return <div>
+        {map1.get('b')} Vs. {map2.get('b')}
+    </div>
+};
+
+export default immutableTEst;
+
 
