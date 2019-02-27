74e860066b35fddcdc41100642d2ee7fb8af2499
shanbin cai <shanbin.cai@gmail.com>
2019-02-27 15:16:57 +0800

    Change Immutable view style

--- src/App/Immutable/Immutable.jsx
+++ src/App/Immutable/Immutable.jsx
@@ -6,7 +6,7 @@ const map1 = Map({ a: 1, b: 2, c:3 });
 const map2 = map1.set('b', 50);
 
 const immutableTEst = () => {
-    return <div>
+    return <div className='App'>
         {map1.get('b')} Vs. {map2.get('b')}
     </div>
 };
